4c0f5f212927ac499f811b69b7e8a8ad9c9d0226
Anushka kushwaha <[anushkakushwaha0201@gmail.com]>
2023-05-17 22:24:39 +0530

    prediction done

--- app/src/main/java/com/example/imagerecog/MainActivity.java
+++ app/src/main/java/com/example/imagerecog/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.imagerecog;
 
+import static org.tensorflow.lite.DataType.UINT8;
+
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,12 +11,20 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.provider.MediaStore;
+import android.renderscript.Element;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.example.imagerecog.ml.MobilenetV110224Quant;
+import com.google.android.gms.fitness.data.DataType;
+
+import org.tensorflow.lite.support.image.TensorImage;
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer;
+
 import java.io.IOException;
+import java.nio.ByteBuffer;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -42,9 +52,54 @@ public class MainActivity extends AppCompatActivity {
                 startActivityForResult(intent, 100);
             }
         });
+        predict.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                img= Bitmap.createScaledBitmap(img, 224,224,true);
+                try {
+                    MobilenetV110224Quant model = MobilenetV110224Quant.newInstance(getApplicationContext());
+
+                    // Creates inputs for reference.
+
+                    TensorBuffer inputFeature0 = TensorBuffer.createFixedSize(new int[]{1, 224, 224, 3},UINT8);
+
+                    TensorImage tensorImage = new TensorImage(UINT8);
+                    tensorImage.load(img);
+                    ByteBuffer byteBuffer = tensorImage.getBuffer();
+
+
+
+
+                    inputFeature0.loadBuffer(byteBuffer);
+
+                    // Runs model inference and gets result.
+                    MobilenetV110224Quant.Outputs outputs = model.process(inputFeature0);
+                    TensorBuffer outputFeature0 = outputs.getOutputFeature0AsTensorBuffer();
+
+                    // Releases model resources if no longer used.
+                    tv.setText(getmax(outputFeature0.getFloatArray())+" ");
+                    model.close();
+                } catch (IOException e) {
+                    // TODO Handle the exception
+                }
+
+            }
+        });
+
 
     }
-
+int getmax(float[] arr)
+{
+    int max=0;
+    for(int i=0;i<arr.length;i++)
+    {
+        if(arr[i]>arr[max])
+        {
+            max=i;
+        }
+    }
+    return max;
+}
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -57,7 +112,6 @@ public class MainActivity extends AppCompatActivity {
             } catch (IOException e) {
                 e.printStackTrace();
             }
-
         }
     }
 }
