d75512075c01c697f5bdaca99b92269e4ceab66d
Anushka kushwaha <[anushkakushwaha0201@gmail.com]>
2023-05-17 15:08:42 +0530

    Selecting media file

--- app/src/main/java/com/example/imagerecog/MainActivity.java
+++ app/src/main/java/com/example/imagerecog/MainActivity.java
@@ -1,14 +1,64 @@
 package com.example.imagerecog;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView imgView;
+    private Button select, predict;
+    private TextView tv;
+    private Bitmap img;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imgView = (ImageView) findViewById(R.id.imageView);
+        tv = (TextView) findViewById(R.id.textView2);
+        select = (Button) findViewById((R.id.button));
+        predict = (Button) findViewById(R.id.button2);
+
+        select.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                startActivityForResult(intent, 100);
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==100)
+        {
+            imgView.setImageURI(data.getData());
+            Uri uri =data.getData();
+            try {
+                img = MediaStore.Images.Media.getBitmap(this.getContentResolver(),uri);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
     }
 }
+
