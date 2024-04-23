45887698cc7282614237479e30be150497b319cb
Anushkaa02 <92162822+Anushkaa02@users.noreply.github.com>
2024-04-23 18:07:07 +0530

    Update inf.py

--- inf.py
+++ inf.py
@@ -27,4 +27,6 @@ interpreter.invoke()
 # The function `get_tensor()` returns a copy of the tensor data.
 # Use `tensor()` in order to get a pointer to the tensor.
 output_data = interpreter.get_tensor(output_details[0]['index'])
+
+
 print(output_data)
